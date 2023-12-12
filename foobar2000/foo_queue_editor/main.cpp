69c1f40e7cf658f49d166c2087f71380795b82f9
fooDayuyu@gmail.com <foodayuyu@gmail.com>
2023-12-12 21:05:56 +0100

    v2.0

--- foobar2000/foo_queue_editor/main.cpp
+++ foobar2000/foo_queue_editor/main.cpp
@@ -11,18 +11,25 @@ DECLARE_COMPONENT_VERSION(
 	// about text, use \n to separate multiple lines
 	// If you don't want any about text, set this parameter to NULL.
 	COMPONENT_NAME_H ": A " COMPONENT_DESC
-	"\n"
-	"\nContributors:\n\n"
-	"Sami Salonen (Queue Contents Editor v0.5.2)\n"
-	"da yuyu (x64 binary Queue Editor v1.0) \n\n"
+	"\n\n"
 	"Version: "FOO_COMPONENT_VERSION"\n"
 	"Compiled: "__DATE__ "\n"
 	"fb2k SDK: "PLUGIN_FB2K_SDK"\n\n"
-	"foo_queue_editor enables the user to edit and view queue contents through an ui element. Both the Default User Interface (DUI) and Columns User Interface (CUI/uie) are supported. As a legacy option, queue contents can also be viewed and edited by a special queue playlist, which is updated automatically with queue contents."
+	"Copyright (c) 2005 Holger Stenger"
+	"\n"
+	"Copyright (c) 2011-2023 Sami Salonen"
+	"\n"
+	"Copyright (c) 2023 Da yuyu"
+	"\n\n"
+	"Contributors:\n\n"
+	"Holger Stenger (Original author of foo_locktest)\n"
+	"Sami Salonen (Original author of Queue Contents Editor up to v0.5.2)\n"
+	"Da yuyu (Queue Editor from v1.0) \n\n"
+	"Queue Editor enables the user to edit and view queue contents through an ui element. Both the Default User Interface (DUI) and Columns User Interface (CUI/uie) are supported. As a legacy option, queue contents can also be viewed and edited by a special queue playlist, which is updated automatically with queue contents."
 	"\n\n"
 	"For more information, please see the Wiki page: " COMPONENT_WIKIPAGEURL
-	"\n\n"
-	"Please use this thread on hydrogenaudio forums to provide feedback, or to report any bugs you might have found: " FORUMURL 
+	//"\n\n"
+	//"Please use this thread on hydrogenaudio forums to provide feedback, or to report any bugs you might have found: " FORUMURL 
 );
 
 // This will prevent users from renaming your component around (important for proper troubleshooter behaviors) or loading multiple instances of it.
